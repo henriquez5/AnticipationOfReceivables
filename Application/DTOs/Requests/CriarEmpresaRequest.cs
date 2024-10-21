9b721671b6733d5b2b724e42f9cba8a7cee4ddc7
henriquez5 <31492149+henriquez5@users.noreply.github.com>
2024-10-21 20:50:48 -0300

    camada application

---
+++ Application/DTOs/Requests/CriarEmpresaRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.DTOs.Requests
+{
+    public class CriarEmpresaRequest
+    {
+        public string CNPJ { get; set; }
+        public string Nome { get; set; }
+        public decimal FaturamentoMensal { get; set; }
+        public string Ramo { get; set; }
+    }
+}
---
+++ Application/DTOs/Requests/CriarNotaFiscalRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Requests
+{
+    public class CriarNotaFiscalRequest
+    {
+        public string? Cnpj { get; set; }
+        public int Numero { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime DataVencimento { get; set; }
+        public int EmpresaId { get; internal set; }
+    }
+}
