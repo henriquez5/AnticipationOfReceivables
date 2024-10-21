e56d1eae00ac35dca8017bfab9ba9577d142bf4d
henriquez5 <31492149+henriquez5@users.noreply.github.com>
2024-10-21 20:53:02 -0300

    camada API

---
+++ ReceivablesAPI/Controllers/EmpresaController.cs
@@ -0,0 +1,43 @@
+﻿using Application.DTOs.Requests;
+using Application.DTOs.Responses;
+using Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReceivablesAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmpresaController : ControllerBase
+    {
+        private readonly IEmpresaService _empresaService;
+
+        public EmpresaController(IEmpresaService empresaService)
+        {
+            _empresaService = empresaService;
+        }
+
+        [HttpGet("obter-todas")]
+        public async Task<IActionResult> ObterTodasEmpresas()
+        {
+            var response = await _empresaService.ObterTodasEmpresas();
+
+            return Ok(response);
+        }
+
+        [HttpGet("obter-todas-id")]
+        public async Task<IActionResult> ObterEmpresaPorId(int id)
+        {
+            var response = await _empresaService.ObterEmpresaPorId(id);
+
+            return Ok(response);
+        }
+
+        [HttpPost("inserir-empresa")]
+        public async Task<IActionResult> InserirEmpresa(CriarEmpresaRequest criarEmpresa)
+        {
+            var response = await _empresaService.InserirEmpresa(criarEmpresa);
+
+            return Ok(response);
+        }
+    }
+}
---
+++ ReceivablesAPI/Controllers/NotaFiscalController.cs
@@ -0,0 +1,43 @@
+﻿using Application.DTOs.Requests;
+using Application.Services;
+using Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReceivablesAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NotaFiscalController : ControllerBase
+    {
+        private readonly INotaFiscalService _notaFiscalService;
+
+        public NotaFiscalController(INotaFiscalService notaFiscalService)
+        {
+            _notaFiscalService = notaFiscalService;
+        }
+
+        [HttpGet("obter-todas")]
+        public async Task<IActionResult> ObterTodasNotasFiscais()
+        {
+            var response = await _notaFiscalService.ObterTodasNotasFiscais();
+
+            return Ok(response);
+        }
+
+        [HttpGet("obter-todas-cnpj")]
+        public async Task<IActionResult> ObterTodasNotasFiscaisPorCNPJ(string cnpj)
+        {
+            var response = await _notaFiscalService.ObterTodasNotasFiscaisPorCNPJ(cnpj);
+
+            return Ok(response);
+        }
+
+        [HttpPost("inserir-nota-fiscal")]
+        public async Task<IActionResult> InserirNotaFiscal(CriarNotaFiscalRequest notafiscal)
+        {
+            var response = await _notaFiscalService.InserirNotaFiscal(notafiscal);
+
+            return Ok(response);
+        }
+    }
+}
