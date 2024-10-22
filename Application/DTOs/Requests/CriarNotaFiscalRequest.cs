40cb2a47e882c90a2f3aa1ecec3c0e27fb94ee59
henriquez5 <31492149+henriquez5@users.noreply.github.com>
2024-10-21 21:50:34 -0300

    Update CriarNotaFiscalRequest.cs

--- Application/DTOs/Requests/CriarNotaFiscalRequest.cs
+++ Application/DTOs/Requests/CriarNotaFiscalRequest.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,15 @@ namespace Application.DTOs.Requests
 {
     public class CriarNotaFiscalRequest
     {
+        [RegularExpression(@"\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2}|\d{14}", ErrorMessage = "CNPJ inválido.")]
         public string? Cnpj { get; set; }
+
         public int Numero { get; set; }
+
         public decimal Valor { get; set; }
+
         public DateTime DataVencimento { get; set; }
+
         public int EmpresaId { get; internal set; }
     }
 }
