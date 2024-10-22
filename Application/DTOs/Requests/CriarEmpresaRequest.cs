21c4f89089d677bce9d703893a996df7debd8dc4
henriquez5 <31492149+henriquez5@users.noreply.github.com>
2024-10-21 21:51:37 -0300

    Update CriarEmpresaRequest.cs

--- Application/DTOs/Requests/CriarEmpresaRequest.cs
+++ Application/DTOs/Requests/CriarEmpresaRequest.cs
@@ -1,10 +1,17 @@
-﻿namespace Application.DTOs.Requests
+using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs.Requests
 {
     public class CriarEmpresaRequest
     {
+        [RegularExpression(@"\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2}|\d{14}", ErrorMessage = "CNPJ inválido.")]
         public string CNPJ { get; set; }
+
+        [Required]
         public string Nome { get; set; }
+
         public decimal FaturamentoMensal { get; set; }
+
         public string Ramo { get; set; }
     }
 }
