32acccb81595dc8c853f985911c7a9b6e13ddb4b
henriquez5 <31492149+henriquez5@users.noreply.github.com>
2024-10-28 21:59:04 -0300

    Update INotaFiscalRepository.cs

--- Infrastructure/Repository/Interfaces/INotaFiscalRepository.cs
+++ Infrastructure/Repository/Interfaces/INotaFiscalRepository.cs
@@ -1,17 +1,17 @@
-﻿using Domain.Entities;
+using Domain.Entities;
 
 namespace Infrastructure.Repository.Interfaces
 {
     public interface INotaFiscalRepository
     {
-        Task<List<NotaFiscal>> ObterTodasNotasFiscais();
+        Task<List<NotaFiscal>?> ObterTodasNotasFiscais();
 
-        Task<NotaFiscal> ObterNotaFiscalPorId(int id);
+        Task<NotaFiscal?> ObterNotaFiscalPorId(int id);
 
         Task<List<NotaFiscal>> ObterTodasNotasFiscaisPorCNPJ(string cnpj);
 
         Task<NotaFiscal> InserirNotaFiscal(NotaFiscal input);
 
-        Task DeletarNotaFiscal(int id);
+        Task<bool> DeletarNotaFiscal(int id);
     }
 }
