919578a1f11b4efd0b8c5ef8fecc0998d07d6b06
henriquez5 <31492149+henriquez5@users.noreply.github.com>
2024-10-28 21:52:58 -0300

    Update CriarEmpresaRequest.cs

--- Application/DTOs/Requests/CriarEmpresaRequest.cs
+++ Application/DTOs/Requests/CriarEmpresaRequest.cs
@@ -4,12 +4,15 @@ namespace Application.DTOs.Requests
 {
     public class CriarEmpresaRequest
     {
-        [RegularExpression(@"\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2}|\d{14}", ErrorMessage = "CNPJ inválido.")]
+        [Required(ErrorMessage = "O CNPJ é obrigatório.")]
+        [RegularExpression(@"^(\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2})$",
+            ErrorMessage = "O CNPJ deve estar no formato XX.XXX.XXX/XXXX-XX")]
         public string CNPJ { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "O Nome é obrigatório.")]
         public string Nome { get; set; }
 
+        [Range(0.01, double.MaxValue, ErrorMessage = "O Faturamento Mensal deve ser maior que 0.")]
         public decimal FaturamentoMensal { get; set; }
 
         public string Ramo { get; set; }
