a694ee20acaabb1fa0bf56f77c577c48483e8ba1
henriquez5 <31492149+henriquez5@users.noreply.github.com>
2024-10-28 21:57:15 -0300

    Update Empresa.cs

--- Domain/Entities/Empresa.cs
+++ Domain/Entities/Empresa.cs
@@ -1,12 +1,19 @@
-﻿namespace Domain.Entities
+using Domain.Enums;
+
+namespace Domain.Entities
 {
     public class Empresa
     {
         public int Id { get; set; }
+
         public string CNPJ { get; set; }
+
         public string Nome { get; set; }
+
         public decimal FaturamentoMensal { get; set; }
-        public string Ramo { get; set; }
+
+        public RamoEmpresa Ramo { get; set; }
+
         public decimal? Limite { get; set; }
 
         public ICollection<NotaFiscal> NotasFiscais { get; set; } = new List<NotaFiscal>();
